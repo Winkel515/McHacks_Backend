74a96433236d27b14ce2f5ac49977997d088df2f
Winkel Yin <winkel.yin@videri.com>
2023-01-28 20:42:19 -0500

    update port

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(
 mongoose.set('strictQuery', false);
 mongoose.connect(DB_URL);
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT;
 
 app.get('/:userId', async (req, res) => {
   const { userId } = req.params;
