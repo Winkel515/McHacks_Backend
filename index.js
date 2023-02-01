791f706473cfe06541cc8fb2747a9035125256bb
Winkel515 <winkel515@hotmail.com>
2023-02-01 12:06:17 -0500

    block new reviews

--- index.js
+++ index.js
@@ -35,52 +35,52 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
   try {
-    const {
-      rating = null,
-      userId = null,
-      comment = null,
-      food = null,
-    } = req.body;
+    // const {
+    //   rating = null,
+    //   userId = null,
+    //   comment = null,
+    //   food = null,
+    // } = req.body;
 
-    const validFood = {
-      Muffin: true,
-      Coffee: true,
-      Bars: true,
-      Wrap: true,
-      'Rice dish': true,
-      Apple: true,
-      'Juice Box': true,
-      Soda: true,
-      'Red Bull': true,
-      Bagel: true,
-      Samosas: true,
-      Banana: true,
-      Ramen: true,
-      Fruitsations: true,
-      Chips: true,
-      Sandwich: true,
-    };
+    // const validFood = {
+    //   Muffin: true,
+    //   Coffee: true,
+    //   Bars: true,
+    //   Wrap: true,
+    //   'Rice dish': true,
+    //   Apple: true,
+    //   'Juice Box': true,
+    //   Soda: true,
+    //   'Red Bull': true,
+    //   Bagel: true,
+    //   Samosas: true,
+    //   Banana: true,
+    //   Ramen: true,
+    //   Fruitsations: true,
+    //   Chips: true,
+    //   Sandwich: true,
+    // };
 
-    if (rating > 5 || rating < 1 || !validFood[food]) {
-      return res.status(400).json({
-        message: 'Get a life',
-      });
-    }
+    // if (rating > 5 || rating < 1 || !validFood[food]) {
+    //   return res.status(400).json({
+    //     message: 'Get a life',
+    //   });
+    // }
 
-    let review = await Review.findOne({ userId, food });
+    // let review = await Review.findOne({ userId, food });
 
-    if (!review) {
-      review = new Review({
-        rating,
-        userId,
-        food,
-      });
-    } else {
-      if (rating) review.rating = rating;
-      if (comment) review.comment = comment;
-    }
+    // if (!review) {
+    //   review = new Review({
+    //     rating,
+    //     userId,
+    //     food,
+    //   });
+    // } else {
+    //   if (rating) review.rating = rating;
+    //   if (comment) review.comment = comment;
+    // }
 
-    await review.save();
+    // await review.save();
     res.send({
       message: 'Success',
     });
